7db2815e5a9850c5f9c797d52d644d08c220c21d
unknown <sahilrawat049@gmail.com>
2024-06-17 16:54:42 +0530

    databse remove same thread for postgress

--- auth/database.py
+++ auth/database.py
@@ -4,10 +4,11 @@ import os
 
 if os.getcwd() == "/home/ubuntu/IvoAuth":
     DATABASE_URL = "postgresql://ivoportal:Psdnj@Eecezc3233r@localhost:5432/ivoportal"
+    engine = create_engine(DATABASE_URL)
 else:
     DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+    
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
