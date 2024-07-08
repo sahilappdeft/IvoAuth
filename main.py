abcbccb86fbff002f00c6c095b4b4eb09703b2de
unknown <sahilrawat049@gmail.com>
2024-07-08 12:45:16 +0530

    error handling

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from auth.database import engine
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
+from auth.database import engine
 from auth.models import Base
 from auth.views import router
 
@@ -9,3 +12,13 @@ app = FastAPI()
 
 app.include_router(router)
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc: RequestValidationError):
+    error_list = []
+    for error in  exc.errors():
+        error_list.append(f"{error['loc'][1]} : {error['msg']}") 
+        
+    return JSONResponse(
+        status_code=400,
+        content={"detail": error_list},
+    )
