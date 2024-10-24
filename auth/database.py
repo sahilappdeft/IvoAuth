df205d88437da1e51e826f5f4c9566792dee5f35
unknown <sahilrawat049@gmail.com>
2024-10-24 19:07:33 +0530

    changes

--- auth/database.py
+++ auth/database.py
@@ -4,12 +4,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
 if os.getcwd() == "/home/ubuntu/IvoAuth":
+    print("*********************")
     # Encode the password
     password = "Psdnj@Eecezc3233r"
     encoded_password = quote_plus(password)
     DATABASE_URL = f"postgresql://ivoportal:{encoded_password}@13.202.134.230:5432/ivoportal"
     engine = create_engine(DATABASE_URL)
 else:
+    print("&&&&&&&&&&&&&&&&&&&&&&&&")
     DATABASE_URL = "sqlite:///./test.db"
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
     
